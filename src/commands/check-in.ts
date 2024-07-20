5637ffa2729fc4a614544e68d7f915b9fa5f0b04
holazz <2418184580@qq.com>
2024-07-20 10:27:28 +0800

    chore: update

--- src/commands/check-in.ts
+++ src/commands/check-in.ts
@@ -1,6 +1,8 @@
+import fsp from 'node:fs/promises'
 import { Contract, Wallet } from 'ethers'
 import pLimit from 'p-limit'
-import { getProvider, retry } from '../utils'
+import dayjs from '../utils/dayjs'
+import { generateWalletTitle, getProvider, retry } from '../utils'
 import logger from '../utils/logger'
 import { resolvedWallets } from '../configs/wallets'
 
@@ -23,6 +25,16 @@ async function checkIn(signer: Wallet) {
       nonce,
     })
     logger.success(signer.address, '签到成功!')
+    await fsp.appendFile(
+      'check.txt',
+      `${dayjs().format('YYYY-MM-DD HH:mm:ss')} ${generateWalletTitle(
+        signer.address,
+      )}\n`.replace(
+        // eslint-disable-next-line no-control-regex
+        /\x1B\[\d+m/g,
+        '',
+      ),
+    )
   } catch (e: any) {
     logger.error(signer.address, e?.error?.reason || 'error')
   }
